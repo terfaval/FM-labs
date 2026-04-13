99c4d5610325d6365f56e1966cdf80737fac65a1
terfaval <mate.fater@gmail.com>
2026-04-13 13:13:06 +0200

    test: fix react import for contact section test

--- components/ContactSection.test.tsx
+++ components/ContactSection.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { describe, expect, it } from "vitest";
 import { fireEvent, render, screen } from "@testing-library/react";
 import { ContactSection } from "./ContactSection";
